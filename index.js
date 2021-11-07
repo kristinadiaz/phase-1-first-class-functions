a94ffe6283392e00156db6a0ad0df947443bf6c2
Kristina Diaz <kristina.diaz84@gmail.com>
2021-11-06 21:19:52 -0600

    commit tree 7cad88a641e1cf99e215344bd0dc8350ffd3a094

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback) {
+    return callback();
+}
+
+function returnsANamedFunction() {
+    return (function named() {});
+}
+
+function returnsAnAnonymousFunction() {
+    return (function() {});
+}
